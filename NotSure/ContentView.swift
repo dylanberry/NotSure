8d205f10b0fca713fcfadd0bc75bc9296a190cc1
Dylan Berry <dylanberry@gmail.com>
2019-12-10 11:17:55 -0500

    Initial commit

---
+++ NotSure/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  NotSure
+//
+//  Created by Dylan Berry on 2019-12-10.
+//  Copyright © 2019 dylanberry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
